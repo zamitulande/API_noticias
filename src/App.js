280f260a87ae956e2a14fc0e675b640100b07adb
zami tulande <hulmartulande@gmail.com>
2021-11-16 09:35:03 -0500

    Proyecto API noticias terminado, curso de React

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
+
+function App() {
+
+   // definir la categoria y noticias
+   const [categoria, guardarCategoria] = useState('');
+   const [noticias, guardarNoticias] = useState([]);
+
+   useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=eff86f09037a4528891194f90a7c8e82`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  },[categoria]);
+
+  return (
+   <Fragment>
+     <Header
+      titulo='Buscador de Noticias'
+      />
+      <div className="container white">
+       <Formulario
+       guardarCategoria={guardarCategoria}
+       /> 
+       <ListadoNoticias
+       noticias={noticias}
+       />
+      </div>
+   </Fragment>
+  );
+}
+
+export default App;
